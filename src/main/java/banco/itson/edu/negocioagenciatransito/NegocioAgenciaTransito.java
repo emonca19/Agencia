8e261cccec5c40e5070b07103848a8d24e4d451c
emonca19 <158860159+emonca19@users.noreply.github.com>
2024-03-15 17:52:24 -0700

    Creacion clase

---
+++ src/main/java/banco/itson/edu/negocioagenciatransito/NegocioAgenciaTransito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package banco.itson.edu.negocioagenciatransito;
+
+/**
+ *
+ * @author elimo
+ */
+public class NegocioAgenciaTransito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
