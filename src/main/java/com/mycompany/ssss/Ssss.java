6890c182f9ba2f742b4efbd841a6eaedcf2cc7cc
PC <PC@DESKTOP-S02NUEJ>
2024-03-16 02:53:22 -0600

    Se agregó la clase IConexion en el paquete interfaces

---
+++ src/main/java/com/mycompany/ssss/Ssss.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ssss;
+
+/**
+ *
+ * @author PC
+ */
+public class Ssss {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
