b9c133567b66f775b0659e56754b4ffcc39a3092
Amós Olguin <PC@DESKTOP-S02NUEJ>
2024-03-21 21:59:20 -0600

    Se modifico la consulta

---
+++ src/main/java/negocio/PersonaConsulta.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package negocio;
+
+import Entidades.PersonaEntidad;
+import java.util.List;
+
+/**
+ *
+ * @author PC
+ */
+public class PersonaConsulta {
+    
+    public PersonaConsulta(){}
+    
+//    public List<PersonaEntidad> consultarListaPersonas(){
+//        
+//        IPersonaDAO personaDAO = new PersonaDAO();
+//        
+//    }
+//    
+}
