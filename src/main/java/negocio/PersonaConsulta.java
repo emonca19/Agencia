5f371d77ac6588426e2e0fb6c93659b89a0cdf36
Amós Olguin <PC@DESKTOP-S02NUEJ>
2024-03-21 22:22:32 -0600

    Se completo el negocio de persona

--- src/main/java/negocio/PersonaConsulta.java
+++ src/main/java/negocio/PersonaConsulta.java
@@ -4,8 +4,13 @@
  */
 package negocio;
 
+import DAOS.IPersonaDAO;
+import DAOS.PersonaDAO;
+import DTO.PersonaGeneradaDTO;
+import DTO.PersonaNuevaDTO;
 import Entidades.PersonaEntidad;
 import java.util.List;
+import modificadores.Convertidor;
 
 /**
  *
@@ -13,12 +18,68 @@ import java.util.List;
  */
 public class PersonaConsulta {
     
-    public PersonaConsulta(){}
+    private final IPersonaDAO personaDAO;
     
-//    public List<PersonaEntidad> consultarListaPersonas(){
-//        
-//        IPersonaDAO personaDAO = new PersonaDAO();
-//        
-//    }
-//    
+    public PersonaConsulta(){
+        
+        personaDAO = new PersonaDAO();
+    
+    }
+    
+    /**
+     * Metodo que llama a persistencia por una lista de las entidades persona
+     * @return listaPersona
+     */
+    public List<PersonaEntidad> listaPersonas(){
+        
+        List<PersonaEntidad> listaPersonas = personaDAO.listaPersonas();
+        
+        return listaPersonas;
+        
+    }
+    
+    /**
+     * Metodo que regresa una lista con las personas que coincidan con el nombre
+     * @param nombres
+     * @return listaPersonasNombre
+     */
+    public List<PersonaEntidad> listaPersonasNombre(String nombres){
+        
+        List<PersonaEntidad> listaPersonasNombre = personaDAO.buscarPorNombre(nombres);
+        
+        return listaPersonasNombre;
+        
+    }
+    
+    /**
+     * Metodo que regresa un dto de una persona buscada por su id
+     * @param id
+     * @return personaGeneradaDTO
+     */
+    public PersonaGeneradaDTO personaPorID(Long id){
+        
+        PersonaEntidad personaEntidad = personaDAO.buscarPorId(id);
+
+        Convertidor convertidor = new Convertidor();
+
+        PersonaGeneradaDTO personaGeneradaDTO = convertidor.entityToDTO(personaEntidad, new PersonaGeneradaDTO());
+        return personaGeneradaDTO;
+       
+    }
+    
+    /**
+     * Metodo que regresa un dto que fue enviado a persistir
+     * @param personaNuevaDTO
+     * @return personaGeneradaDTO
+     */
+    public PersonaGeneradaDTO agregarPersona(PersonaNuevaDTO personaNuevaDTO){
+        
+        Convertidor convertidor = new Convertidor();
+        PersonaEntidad personaEntidad = convertidor.DTOToEntidad(personaNuevaDTO, new PersonaEntidad());
+        personaEntidad = personaDAO.agregarPersona(personaEntidad);
+        PersonaGeneradaDTO personaGeneradaDTO = convertidor.entityToDTO(personaEntidad, new PersonaGeneradaDTO());
+        return personaGeneradaDTO;
+        
+    }
+    
 }
