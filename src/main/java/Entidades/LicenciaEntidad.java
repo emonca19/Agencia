9ecd5e6ee402e5a7605a2ebe6761a0fd077a21c7
Amós Olguin <PC@DESKTOP-S02NUEJ>
2024-03-21 18:26:23 -0600

    Se modifico la licencia y se eliminaron atributos repetidos con las demas hijas

--- src/main/java/Entidades/LicenciaEntidad.java
+++ src/main/java/Entidades/LicenciaEntidad.java
@@ -7,59 +7,39 @@ import javax.persistence.Entity;
 @Entity
 public class LicenciaEntidad extends TramiteEntidad implements Serializable {
     
-    @Column(name = "estado", nullable = false)
-    private int estado;
-    
     @Column(name = "vigencia", nullable = false)
     private int vigencia;
     
-    @Column(name = "costo", nullable = false)
-    private int costo;
-    
-//    
-//    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REMOVE})
-//    @JoinColumn(name = "idPersona")
-//    private PersonaEntidad cliente;
+    @Column(name = "folio", nullable = false)
+    private int folio;
 
     public LicenciaEntidad() {
     }
 
-    public LicenciaEntidad(int estado, int vigencia, int costo) {
-        this.estado = estado;
+    public LicenciaEntidad(int vigencia, int folio) {
         this.vigencia = vigencia;
-        this.costo = costo;
+        this.folio = folio;
+    }
+
+    public int getVigencia() {
+        return vigencia;
+    }
+
+    public void setVigencia(int vigencia) {
+        this.vigencia = vigencia;
+    }
+
+    public int getFolio() {
+        return folio;
     }
     
-    public int getEstado() {
-        return estado;
-    }
-
-    public void setEstado(int estado) {
-        this.estado = estado;
-    }
-//    
-//    public PersonaEntidad getPersona() {
-//        return cliente;
-//    }
-//
-//    public void setCliente(PersonaEntidad cliente) {
-//        this.cliente = cliente;
-//    }
-
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("LicenciaEntidad{");
-//        sb.append("id=").append(id);
-        sb.append(", estado=").append(estado);
         sb.append(", vigencia=").append(vigencia);
-        sb.append(", costo=").append(costo);
         sb.append('}');
         return sb.toString();
-    }
-
-    public void setPersona(PersonaEntidad persona) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
 }
