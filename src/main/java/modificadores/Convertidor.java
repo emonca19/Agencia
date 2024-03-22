2e61bf60f917fbe3937352638b3e3bea99cf9aa9
Amós Olguin <PC@DESKTOP-S02NUEJ>
2024-03-21 23:37:51 -0600

    Se agregaron comentarios en convertidores

--- src/main/java/modificadores/Convertidor.java
+++ src/main/java/modificadores/Convertidor.java
@@ -12,32 +12,31 @@ import java.lang.reflect.Field;
  */
 public class Convertidor{
 
+    /**
+     * Metodo que convierte los DTOs a Entidades de su mismo tipo
+     * @param <Entity>
+     * @param <DTO>
+     * @param dto
+     * @param entity
+     * @return la entidad convertide de un dto
+     */
     public <Entity, DTO> Entity DTOToEntidad(DTO dto, Entity entity) {
     
         Class<?> claseDTO = dto.getClass();
         Class<?> claseEntity = entity.getClass();
-        
         Field[] camposDTO = claseDTO.getDeclaredFields();
         
         for(Field campoDTO: camposDTO){
             
             try {
-                // Obtener el nombre del campo
                 String nombreCampoDto = campoDTO.getName();
-
-                // Obtener el valor del campo en DTO
-                campoDTO.setAccessible(true); // Hacer accesible el campo incluso si es privado
+                campoDTO.setAccessible(true);
                 Object valorCampoDto = campoDTO.get(dto);
-
-                // Obtener el campo correspondiente en la entidad
                 Field campoEntity = claseEntity.getDeclaredField(nombreCampoDto);
-                campoEntity.setAccessible(true); // Hacer accesible el campo incluso si es privado
-
-                // Establecer el valor del campo en la entidad
+                campoEntity.setAccessible(true);
                 campoEntity.set(entity, valorCampoDto);
-                
             } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace(); // Manejar las excepciones según sea necesario
+                e.printStackTrace();
             }
         }
         
@@ -45,38 +44,37 @@ public class Convertidor{
         
     }
 
+    /**
+     * Metodo que convierte las entidades en dtos de su mismo tipo
+     * @param <Entity>
+     * @param <DTO>
+     * @param entity
+     * @param dto
+     * @return el dto convertido de la entidad
+     */
     public <Entity, DTO> DTO entityToDTO(Entity entity, DTO dto) {
     
         Class<?> claseEntity = entity.getClass();
         Class<?> clasesDTO = dto.getClass();
-        
         Field[] camposEntity = claseEntity.getDeclaredFields();
         
         for(Field campoEntity: camposEntity){
             
             try {
-                // Obtener el nombre del campo
                 String nombreCampoEntity = campoEntity.getName();
-
-                // Obtener el valor del campo en DTO
-                campoEntity.setAccessible(true); // Hacer accesible el campo incluso si es privado
+                campoEntity.setAccessible(true);
                 Object valorCampoEntity = campoEntity.get(entity);
-
-                // Obtener el campo correspondiente en la entidad
-                
                 Field campoDTO = clasesDTO.getDeclaredField(nombreCampoEntity);
                 if(valorCampoEntity != campoDTO){
                     
                     break;
                     
                 }
-                campoDTO.setAccessible(true); // Hacer accesible el campo incluso si es privado
-
-                // Establecer el valor del campo en la entidad
+                campoDTO.setAccessible(true);
                 campoDTO.set(dto, valorCampoEntity);
                 
             } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace(); // Manejar las excepciones según sea necesario
+                e.printStackTrace();
             }
         }
         
